4be7eb788eaea526a4f0a043f16d82397abf60cd
赵盼龙 <740058249@qq.com>
2022-09-22 12:12:23 +0800

    完成条目间距书写

--- PicShowLib/src/main/java/com/zhaopanlong/picshowlib/GridPicShowLayout.kt
+++ PicShowLib/src/main/java/com/zhaopanlong/picshowlib/GridPicShowLayout.kt
@@ -1,7 +1,11 @@
 package com.zhaopanlong.picshowlib
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,18 +13,25 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.fondesa.recyclerviewdivider.dividerBuilder
 
 class GridPicShowLayout : RecyclerView {
     var maxLength = 6
     val spanCount = 3
+    private var spaceSize = 20
     val imagePaths = arrayListOf<String>()
     val mAdapter = GridPicAdapter()
 
-   private var mPlusOnClickListener: OnClickListener? = null
+    private var mPlusOnClickListener: OnClickListener? = null
 
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
         layoutManager = GridLayoutManager(context, spanCount)
         adapter = mAdapter
+       context.dividerBuilder()
+            .color(Color.TRANSPARENT)
+            .size(spaceSize, TypedValue.COMPLEX_UNIT_DIP)
+            .build()
+            .addTo(this)
     }
 
 
@@ -82,9 +93,15 @@ class GridPicShowLayout : RecyclerView {
         mAdapter.notifyDataSetChanged()
     }
 
-    fun setOnPlusClickListen(listener: OnClickListener){
+    fun setOnPlusClickListen(listener: OnClickListener) {
         mPlusOnClickListener = listener
     }
+
+    fun setSpaceSize(size: Int) {
+        spaceSize = size
+        mAdapter.notifyDataSetChanged()
+    }
+
 
     inner class IteamViewVh(view: View) : ViewHolder(view) {
 
@@ -95,4 +112,17 @@ class GridPicShowLayout : RecyclerView {
     inner class PlusIteamVh(view: View) : ViewHolder(view) {
 
     }
+
+    inner class SpacesItemDecoration(val space: Int) : RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            outRect.right = space
+            Log.d("GridPicShowLayout","view的条目id="+parent.getChildLayoutPosition(view))
+//            if ((parent.getChildLayoutPosition(view)+1) % spanCount == 0) {
+//                outRect.right = 0
+//            }
+        }
+    }
+
+
 }
